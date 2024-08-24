f6c502edb282782058b482923eed9faf32937a25
Adriancas28 <castroadrian1228@gmail.com>
2024-08-24 16:16:44 -0500

    mmmm

--- obj/Debug/net8.0/PC01_PRO.AssemblyInfo.cs
+++ obj/Debug/net8.0/PC01_PRO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PC01_PRO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+907af336eb1cef41c831de5a5f0c7a4aa52a3a02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf9786ad2382678c24189dc4eb3a1cf60875e8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PC01_PRO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PC01_PRO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
